9427c168c3a28e3ab20c7daa7a87bd7037eb9b70
Kelvin Kakugawa <kakugawa@gmail.com>
2010-10-15 08:33:20 +0000

    WIP: cleaned up test

--- test/unit/org/apache/cassandra/utils/BigBloomFilterTest.java
+++ test/unit/org/apache/cassandra/utils/BigBloomFilterTest.java
@@ -34,10 +34,7 @@ public class BigBloomFilterTest
     public void testSerialize() throws IOException
     {
         bf.add("a");
-System.out.println("testSerialize: 0: " + bf.toString());
-System.out.println("testSerialize: 1: " + bf.isPresent("a"));
         assert bf.isPresent("a");
-System.out.println("testSerialize: 2: " + bf.isPresent("b"));
         assert !bf.isPresent("b");
 
         DataOutputBuffer out = new DataOutputBuffer();
@@ -47,25 +44,18 @@ System.out.println("testSerialize: 2: " + bf.isPresent("b"));
         Filter f2 = BloomFilter.serializer().deserialize(new DataInputStream(in));
 
         BigBloomFilter origBF = (BigBloomFilter)bf;
-        BigBitSet origBBS = origBF.bigFilter();
         BigBloomFilter newBF  = (BigBloomFilter)f2;
-        BigBitSet newBBS  = newBF.bigFilter();
+        assert origBF.hashCount == newBF.hashCount;
+        assert origBF.filter.size() == newBF.filter.size();
+
+        BigBitSet origBBS = origBF.filter;
+        BigBitSet newBBS  = newBF.filter;
         for (int i=0; i < origBBS.buckets.length; i++)
         {
-System.out.println("testSerialize: X1: orig: " + origBBS.buckets[i]);
-System.out.println("testSerialize: X2: new:  " + newBBS.buckets[i]);
             assert origBBS.buckets[i].equals(newBBS.buckets[i]) : "BBS index wrong: " + i;
         }
-System.out.println("testSerialize: Y1: orig: " + origBF.numBits);
-System.out.println("testSerialize: Y2: new:  " + newBF.numBits);
 
-System.out.println("testSerialize: Z1: orig: " + origBF.numHashes);
-System.out.println("testSerialize: Z2: new:  " + newBF.numHashes);
-
-System.out.println("testSerialize: A: " + f2.toString());
-System.out.println("testSerialize: B: " + f2.isPresent("a"));
         assert f2.isPresent("a");
-System.out.println("testSerialize: C: " + f2.isPresent("b"));
         assert !f2.isPresent("b");
     }
 }
